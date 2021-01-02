7f62b7b5b84a4d6431ac4109ca30956cc91a8d3e
Mokume1Shine <tuetuti100@gmail.com>
2021-01-02 20:20:34 +0900

    first voice

--- template.py
+++ template.py
@@ -1,33 +1,10 @@
 import discord
 from discord.ext import commands
 from discord.ext import tasks
-import feedparser
 
 client = discord.Client()
-token=#token (int)自分のはファイル読み込みにした
+token="NjM0Nzg2NzM2NjY0NDc3NzA2.XanlNw.gz3P2RL1f_trXeNR4xxFgOY_raA"
 
-guri_rss_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCM1VDkI02dK0BV-e8nyYobQ'
-guri_rss_link=[]
-
-#rss
-@tasks.loop(seconds=60)
-async def getRSS():
-    global guri_rss_url
-    global guri_rss_link
-    if len(guri_rss_link)==0:#起動時一回目の時
-        rss_reply=feedparser.parse(guri_rss_url)
-        guri_rss_link.append(rss_reply.entries[0]['link'])
-        guri_rss_link.append(rss_reply.entries[1]['link'])
-        guri_rss_link.append(rss_reply.entries[2]['link'])
-    elif not rss_reply.entries[0]['link'] in guri_rss_link:
-        rss_reply=feedparser.parse(guri_rss_url)
-        guri_rss_link[0]=rss_reply.entries[0]['link']
-        guri_rss_link[1]=rss_reply.entries[1]['link']
-        guri_rss_link[2]=rss_reply.entries[2]['link']
-        me="guriの動画が更新されました\n"+guri_rss_link[0]
-        if getBot()=="testbot":
-            await client.get_channel(テキストチャンネルのID).send(me)
-    
 #起動時
 @client.event
 async def on_ready():
@@ -35,7 +12,7 @@ async def on_ready():
     print('name:',client.user.name)
     print('id:',client.user.id)
     await client.change_presence(activity=discord.Game(name="Bot"))
-    getRSS.start()
     print('------')
+    await client.get_channel(794872991037128704).send("Hello Discord World (φωφ)")
 
 client.run(token)
