522f476f1c9b93adde62ffa4121b236d4b799ac7
nafu4869 <61410350+nafu4869@users.noreply.github.com>
2022-02-06 23:49:43 +0900

    Update voiceStateUpdate.js

--- bot_system/voiceStateUpdate.js
+++ bot_system/voiceStateUpdate.js
@@ -15,6 +15,11 @@ const handler = (oldStatus,newStatus) => {
         messagepost.send_message(channel,`${newStatus.member.displayName} が ${newStatus.channel} で画面共有を開始しました`)
     }
 
+    if (oldStatus.selfVideo!=newStatus.selfVideo && newStatus.selfVideo==true){
+        const channel=options.getvoicedefaultchannel(newStatus.guild["id"],newStatus.channelId)
+        messagepost.send_message(channel,`${newStatus.member.displayName} が ${newStatus.channel} でカメラ共有を開始しました`)
+    }
+
 }
 
 module.exports = {name, handler}
