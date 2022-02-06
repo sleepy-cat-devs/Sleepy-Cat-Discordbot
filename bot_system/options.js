541769a0515042490f9ff88a69ab8662309c6154
guriniasu <48523975+guriniasu@users.noreply.github.com>
2022-02-06 19:06:02 +0900

    Bugfix
    「do not overwrite」コミット時の変更により，取得済みチャンネルのデータをファイルから読み込んでおらず，データがなかった問題を修正
    comitID:173a43d1724aad4fca7a0b68b58a26dec709ba9d

--- bot_system/options.js
+++ bot_system/options.js
@@ -6,9 +6,10 @@ exports.client
 exports.optionsdir
 
 exports.getvoicedefaultchannel=(guildid,channelid)=>{
+    console.log(guildid,channelid)
     for(const vc_ch of this.guild_data[guildid]["GUILD_VOICE"]){
         if(vc_ch["ch_id"]==channelid){
-            let ch=this.client.channels.cache.get(vc_ch["default_textch"]["ch_id"])
+            const ch=this.client.channels.cache.get(vc_ch["default_textchid"])
             return ch
         }
     }
@@ -45,29 +46,29 @@ exports.startup=()=>{
     //サーバーデータの取得
     d=Object.keys(this.guild_data)
     for(let i=0;i<d.length;i++){
-        this.guild_data[d[i]]["GUILD_TEXT"]=[]
-        this.guild_data[d[i]]["GUILD_VOICE"]=[]
-        const guild=this.client.guilds.cache.get(d[i])
-        const syschid=guild.systemChannelId
-        const channels=guild.channels.cache.map(a => [a.type,a.id,a.name])
-        //console.log(channels)
-        for(let j=0;j<channels.length;j++){
-            if(channels[j][0]=="GUILD_TEXT")
-                this.guild_data[d[i]]["GUILD_TEXT"].push({"ch_id":channels[j][1],"name":channels[j][2]})
-            else if(channels[j][0]=="GUILD_VOICE")
-                this.guild_data[d[i]]["GUILD_VOICE"].push({"ch_id":channels[j][1],"name":channels[j][2]})
-        }
-        for(let j=0;j<this.guild_data[d[i]]["GUILD_VOICE"].length;j++){
-            this.guild_data[d[i]]["GUILD_VOICE"][j]["default_textch"]=syschid
-        }
-    }
-
-    //サーバーオプションデータの出力
-    for(let i=0;i<d.length;i++){
+        //サーバーオプションデータの出力、データ取得済みチャンネルはパス
         if(fs.existsSync(this.optionsdir+"guilds/"+d[i]+".json")){
             console.log(this.optionsdir+"guilds/"+d[i]+".json\" is found")
+            this.guild_data[d[i]]=JSON.parse(fs.readFileSync(this.optionsdir+"guilds/"+d[i]+".json", 'utf8'));
+            console.log(this.guild_data)
+            break
         }else{
             console.log(this.optionsdir+"guilds/"+d[i]+".json\" is not found")
+            this.guild_data[d[i]]["GUILD_TEXT"]=[]
+            this.guild_data[d[i]]["GUILD_VOICE"]=[]
+            const guild=this.client.guilds.cache.get(d[i])
+            const syschid=guild.systemChannelId
+            const channels=guild.channels.cache.map(a => [a.type,a.id,a.name])
+            //console.log(channels)
+            for(let j=0;j<channels.length;j++){
+                if(channels[j][0]=="GUILD_TEXT")
+                    this.guild_data[d[i]]["GUILD_TEXT"].push({"ch_id":channels[j][1],"name":channels[j][2]})
+                else if(channels[j][0]=="GUILD_VOICE")
+                    this.guild_data[d[i]]["GUILD_VOICE"].push({"ch_id":channels[j][1],"name":channels[j][2]})
+            }
+            for(let j=0;j<this.guild_data[d[i]]["GUILD_VOICE"].length;j++){
+                this.guild_data[d[i]]["GUILD_VOICE"][j]["default_textchid"]=syschid
+            }
             fs.writeFileSync(this.optionsdir+"guilds/"+d[i]+".json",JSON.stringify(this.guild_data[d[i]],null,2))
         }
         
