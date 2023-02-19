c27b95032da8eaaf17f3c99fc64ea4301041dfb0
Mokume1Shine <tuetuti100@gmail.com>
2023-02-19 18:34:29 +0900

    Update channelDelete.js

--- bot_system/events/channelDelete.js
+++ bot_system/events/channelDelete.js
@@ -1,17 +1,19 @@
 //channelDelete.js
 
-//チャンネルが作成されたイベントを取得
+//チャンネルが削除されたイベントを取得
 //bot用データからチャンネルデータを削除する指示
 
 const path = require("path")
-const { ChannelType } = require("discord.js")
+const {
+    ChannelType
+} = require("discord.js")
 
 const options = require("../options")
 
 const name = path.basename(__filename, ".js")
 
 const handler = (channel) => {
-    if (channel.type == ChannelType.GuildText)//ChannelType.GuildText = 0
+    if (channel.type == ChannelType.GuildText) //ChannelType.GuildText = 0
         options.channel_data_update(1, "GUILD_TEXT", channel)
     else if (channel.type == ChannelType.GuildVoice)
         options.channel_data_update(1, "GUILD_VOICE", channel)
