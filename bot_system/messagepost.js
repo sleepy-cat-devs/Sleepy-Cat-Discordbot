318cf940898f729fe2427724f9db09a7c67ba07e
Mokume1Shine <tuetuti100@gmail.com>
2023-06-22 19:38:39 +0900

    [fix] メッセージ送信先のチャンネルが未定義だった場合にエラー落ちしていたバグを解決

--- bot_system/messagepost.js
+++ bot_system/messagepost.js
@@ -6,6 +6,10 @@
 const options = require("./options")
 
 exports.send_message = (channel, mes) => {
+    //未定義のチャンネルは処理を中断
+    if (channel===undefined){
+        return
+    }
     if (!options.isrelease) {
         //テスト中はコンソールにログが出る
         console.log("チャンネル：" + channel + "\n" + mes + " を投稿しようとしました")
