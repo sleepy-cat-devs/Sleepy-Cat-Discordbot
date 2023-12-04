1baece677c3f9f02f071767046b4326f022e7876
Mokume 1Shine <tuetuti100@gmail.com>
2023-12-04 22:35:33 +0900

    ボイチャ通知DB設計を作成

--- docs/praxi-db.sql
+++ docs/praxi-db.sql
@@ -5,15 +5,17 @@
 -- 外部キー制約を有効化
 PRAGMA foreign_keys=true;
 
--- ギルドテーブルを作成
-CREATE TABLE guilds(
-    id text primary key
+-- サーバーテーブルを作成
+CREATE TABLE m_servers(
+    id integer primary key,
+    guild_real_id text not null
 );
 
 -- ボイチャの通知テキストチャンネル指定テーブル
-CREATE TABLE notify_channels(
-    voice_channel_id text primary key,
-    text_channel_id text not null,
-    guild_id text not null,
-    foreign key (guild_id) references guilds(id) 
+CREATE TABLE m_notify_channels(
+    voice_id text primary key,
+    text_id text not null,
+    server_id integer not null,
+    is_notify integer default 1,
+    foreign key (server_id) references m_servers(id) 
 );
